c542adc5e09ad3554693314b7e9b88796aa5f3b8
Jeong Yeon Jae <jyj010818@naver.com>
2022-12-13 00:02:37 +0900

    Clone

--- clone-instagram/src/components/Login/Login.jsx
+++ clone-instagram/src/components/Login/Login.jsx
@@ -5,30 +5,54 @@ import './Login.scss';
 const Login = () => {
     const [id, setId] = useState('');
     const [pw, setPw] = useState('');
+    const [flag, setFlag] = useState(true);
 
-    const onChangeId = e => {
+    const inputId = e => {
         setId(e.target.value);
     };
 
-    const onChangePw = e => {
+    const inputPw = e => {
         setPw(e.target.value);
     };
 
+    const ispassed = () => {
+        return id.includes('@') && pw.length > 4
+            ? setFlag(false) : setFlag(true);
+    };
+
+
+
     return (
-        <div className="Login">
-            <div className="Login-form">
-                <input
-                    className='Login-input'
-                    type="text"
-                    placeholder='전화번호, 사용자 이름 또는 이메일'
-                    onChange={onChangeId}
-                   />
-                <input
-                    className='Login-input'
-                    type="text"
-                    placeholder='비밀번호'
-                    onChange={onChangePw}
-                   />
+        <div className='main'>
+            <div className="loginForm">
+            <div>
+                <img className='loginlogo' alt = "instalogo" src = "img/instagramlogo.png"/>
+            </div>
+                <div>
+                    <input 
+                        type = "text"
+                        placeholder = " 전화번호 사용자 이름 또는 이메일"
+                        className='loginInput'
+                        onChange = {inputId}
+                        onKeyUp = {ispassed}
+                    />
+                </div>
+                <div>
+                    <input
+                        type= "password"
+                        placeholder=' 비밀번호'
+                        className='loginInput'
+                        onChange={inputPw}
+                        onKeyUp = {ispassed}
+                    />
+                </div>
+                <div>
+                    <button
+                        type='button'
+                        className='loginButton'
+                        disabled={flag}
+                    >로그인</button>
+                </div>
             </div>
         </div>
     );
