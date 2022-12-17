dbf07daf4a050bd485b12a42608460fa3c3c59b5
Jeong Yeon Jae <jyj010818@naver.com>
2022-12-17 23:25:23 +0900

    Clone

--- clone-instagram/src/components/Login/Login.jsx
+++ clone-instagram/src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ import './Login.scss';
 const Login = () => {
     const footContent = ['Meta ', '소개 ', '블로그 ', '채용 정보 ', '도움말 ', 'API ',
                   '개인정보처리방침 ', '약관 ', '인기 계정 ', '해시태그 ', '위치 ', 'Instagram Lite ', 
-                  '연락처 업데이트 & 비사용자'];
+                  '연락처 업데이트&비사용자'];
     const [id, setId] = useState('');
     const [pw, setPw] = useState('');
     const [flag, setFlag] = useState(true);
