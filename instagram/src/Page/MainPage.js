e4bcf21632d221c8919a62ccacd3e03a3c471de9
Jeong Yeon Jae <jyj010818@naver.com>
2023-01-09 18:16:15 +0900

    MainPage

--- instagram/src/Page/MainPage.js
+++ instagram/src/Page/MainPage.js
@@ -6,11 +6,33 @@ import Menu from '../component/Menu';
 import StoryList from '../component/StoryList';
 
 const MainPage = () => {
+    const contents = [
+        {
+            id: 1,
+            image: '../img/Dog1.jpeg'
+        },
+        {
+            id: 2,
+            image: '../img/Dog2.jpeg'
+        },
+        {
+            id: 3,
+            image: '../img/Dog3.jpeg'
+        },
+        {
+            id: 4,
+            image: '../img/Dog4.jpeg'
+        },
+        {
+            id: 5,
+            image: '../img/Dog5.jpeg'
+        }
+    ]
     return (
         <div>
             <Menu/>
             <StoryList/>
-            <PostList/>
+            <PostList contents={contents}/>
             <AsideBar/>
         </div>
     );
