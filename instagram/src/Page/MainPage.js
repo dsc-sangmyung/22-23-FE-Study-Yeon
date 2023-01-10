026d150f24f03fd582207ade1a438d87e4f0426f
Jeong Yeon Jae <jyj010818@naver.com>
2023-01-10 19:57:36 +0900

    MainPage

--- instagram/src/Page/MainPage.js
+++ instagram/src/Page/MainPage.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import '../css/MainPage.scss';
-import AsideBar from '../component/AsideBar';
-import PostList from '../component/PostList';
-import Menu from '../component/Menu';
-import StoryList from '../component/StoryList';
+import React from "react";
+import "../css/MainPage.scss";
+import AsideBar from "../component/AsideBar";
+import PostList from "../component/PostList";
+import Menu from "../component/Menu";
 
 const MainPage = () => {
-    const contents = [
-        {
-            id: 1,
-            image: '../img/Dog1.jpeg'
-        },
-        {
-            id: 2,
-            image: '../img/Dog2.jpeg'
-        },
-        {
-            id: 3,
-            image: '../img/Dog3.jpeg'
-        },
-        {
-            id: 4,
-            image: '../img/Dog4.jpeg'
-        },
-        {
-            id: 5,
-            image: '../img/Dog5.jpeg'
-        }
-    ]
-    return (
-        <div>
-            <Menu/>
-            <StoryList/>
-            <PostList contents={contents}/>
-            <AsideBar/>
-        </div>
-    );
+  const contents = [
+    {
+      id: 1,
+      image: "../img/Dog1.jpeg",
+    },
+    {
+      id: 2,
+      image: "../img/Dog2.jpeg",
+    },
+    {
+      id: 3,
+      image: "../img/Dog3.jpeg",
+    },
+    {
+      id: 4,
+      image: "../img/Dog4.jpeg",
+    },
+    {
+      id: 5,
+      image: "../img/Dog5.jpeg",
+    },
+  ];
+  return (
+    <div className="main">
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="feed">
+        <PostList contents={contents} />
+      </div>
+      <div className="aside">
+        <AsideBar />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
