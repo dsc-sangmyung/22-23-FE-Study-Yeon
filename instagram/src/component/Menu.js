8de44e3391d1df4bd4fc182cc69174ef51a794b0
Jeong Yeon Jae <jyj010818@naver.com>
2023-01-13 18:21:55 +0900

    instagram

--- instagram/src/component/Menu.js
+++ instagram/src/component/Menu.js
@@ -8,14 +8,14 @@ import { GoDiffAdded } from "react-icons/go";
 const Menu = () => {
     return (
      <div className='menu'>
-        <li> <AiFillHome className='icon'/><a className='text'>홈</a></li>
-        <li> <AiOutlineSearch /><a className='text'>검색</a></li>
-        <li><IoIosCompass /><a className='text'>탐색 탭</a></li>
-        <li><IoIosPaperPlane /><a className='text'>메시지</a></li>
-        <li><IoMdHeartEmpty /><a className='text'>알림</a></li>
-        <li><GoDiffAdded /><a className='text'>만들기</a></li>
-        <li><IoIosRadioButtonOff /><a className='text'>프로필</a></li>
-        <li><IoIosReorder /><a className='text'>더보기</a></li>
+        <li> <button><AiFillHome className='homeicon'/><a className='text'>홈</a></button></li>
+        <li> <button><AiOutlineSearch className='searchicon'/><a className='text'>검색</a></button></li>
+        <li><button><IoIosCompass className='compassicon'/><a className='text'>탐색 탭</a></button></li>
+        <li><button><IoIosPaperPlane className='planeicon'/><a className='text'>메시지</a></button></li>
+        <li><button><IoMdHeartEmpty className='hearticon'/><a className='text'>알림</a></button></li>
+        <li><button><GoDiffAdded className='addicon'/><a className='text'>만들기</a></button></li>
+        <li><button><IoIosRadioButtonOff className='profileicon'/><a className='text'>프로필</a></button></li>
+        <li><button><IoIosReorder className='extraicon'/><a className='text'>더보기</a></button></li>
      </div>
     );
 };
